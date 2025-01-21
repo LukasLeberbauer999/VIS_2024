6bf991b15ce35dca44ad37f095f8dc79520fd233
LukasLeberbauer999 <s2310566013@fhooe.at>
2025-01-21 11:19:13 +0100

    1

--- Aufgabe_3/qt_widget.py
+++ Aufgabe_3/qt_widget.py
@@ -1,7 +1,7 @@
 import vtk
-import QVTKRenderWindowInteractor as QTVTK
+import QVTKRenderWindowInteractor as QVTK
 
-class Widget(QTVTK.QVTKRenderWindowInteractor):
+class Widget(QVTK.QVTKRenderWindowInteractor):
     def __init__(self):
         super().__init__()
 
@@ -10,6 +10,13 @@ class Widget(QTVTK.QVTKRenderWindowInteractor):
         self.renderer.SetBackground(1.0, 1.0, 1.0) # weißer Hintergrund
         self.GetRenderWindow().AddRenderer(self.renderer)
 
+        # Text-Annotation hinzufügen
+        self.text_actor = vtk.vtkTextActor()
+        self.text_actor.SetPosition(10, 10)  # Position: links unten
+        self.text_actor.GetTextProperty().SetFontSize(14)
+        self.text_actor.GetTextProperty().SetColor(0, 0, 0)  # Schwarzer Text
+        self.renderer.AddActor2D(self.text_actor)
+
     def update_renderer(self, model):
         """Aktualisiert den Renderer mit einem neuen Modell."""
         #self.renderer.RemoveAllViewProps()  # Entfernt alte Modelle
@@ -17,3 +24,10 @@ class Widget(QTVTK.QVTKRenderWindowInteractor):
         self.renderer.ResetCamera()  # Kamera zurücksetzen
         self.GetRenderWindow().Render()  # Rendern des aktualisierten Fensters
 
+    def update_text_actor(self, text):
+        """Aktualisiert den Text des Text-Actors."""
+        self.text_actor.SetInput(text)  # Text setzen
+        self.GetRenderWindow().Render()  # Neu rendern
+
+    def GetRenderer(self):
+        return self.renderer
